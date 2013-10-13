7e991cb6040046f347fdbb4ca8604b95c6a98761
Kyle Ferrio <kferrio [at] gmail [dot] com>
2013-10-13 10:32:35 -0700

    Allow whitespace in regexp; clean up comments and readme.

--- README.md
+++ README.md
@@ -3,7 +3,11 @@ mint-beyond-compare
 
 Python glue to add Beyond Compare context menus to the Mint file browser
 
-It's only right that the world's best file browser should support the world's best diff/merge tool! Unfortunately, as of this writing, Scooter (http://www.scootersoftware.com) has taken a binary approach to shell integration.  Fortunately, Scooter made Beyond Compare scriptable. This hack sets up and manages nemo action files to support both simultaneous and left-then-right selections. 
+It's only right that the world's best file browser should support the world's best diff/merge tool! 
+
+This python hack sets up and manages nemo action files to support both simultaneous selection -- viz., compare these two objects -- and left-then-right selections -- viz., compare this object to that object -- for Nemo on Linux Mint.
+
+Scooter (http://www.scootersoftware.com) has taken a binary approach to shell integration. That approach has the advantage of simplifying installation for multiple licensed users. Unfortunately, it doesn't always work.  And supporting a combinatoric beastiary of Linux OS and shell choices seems like a lot to ask of a presumably small company. Fortunately, Scooter made Beyond Compare scriptable. 
 
 Yes, this repo probably should have been called nemo-beyond-compare. But go load Mint anyway. You'll be glad you did.
 
@@ -18,10 +22,12 @@ Installation
 
  4. Enjoy
 
-Dependencies
-------------
+Tested With...
+--------------
  Beyond Compare 3
 
- Python 2.x
+ Python 2.7
 
- Nemo
+ Nemo 1.8.3
+
+ Linux Mint 15 (Cinnamon on Olivia)
--- helper.py
+++ helper.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python
+#
+# Nemo integration for Beyond Compare 3
+#
+# Installation for Linux Mint:
+#  1) Copy kit to ~/.local/share/nemo/actions
+#  2) chmod +x helper.py #this script)
+#  3) ./helper.py  # one-time setup takes exactly 0 arguments
+
 import os
 import sys
 import errno
 import re
 
-# path for nemo actions; needed because working directory is otherwise the current nemo target
+# Define path for nemo actions. 
+# Otherwise, working directory would default to the current Nemo folder.
 acts = os.path.expanduser('~/.local/share/nemo/actions')
 act = os.path.join(acts, 'bc-half.nemo_action')
+# Define paths for templates for actions.
 act_lhs = os.path.join(acts, 'bc-lhs.na')
 act_rhs = os.path.join(acts, 'bc-rhs.na')
 
-
-# path for holding the lhs state 
+# Define the path for holding the lhs state.
 temp = os.path.expanduser('~/tmp/bcompare')
 state = os.path.join(temp, 'state.txt')
 
 # The nemo action must pass the helper script exactly one argument.
-
 if len(sys.argv)==2:
 	if len(sys.argv[1])>0:
 		if os.path.exists(state):
-			# build and execute the command
+			# Build and execute the BC command *after* cleaning up.
 			rhs = sys.argv[1]
 			f = open(state,'r')
 			lhs = f.readline().strip()
@@ -31,24 +39,23 @@ if len(sys.argv)==2:
 			os.execlp('bcompare', 'bcompare', lhs, rhs)
 
 		else:
-			# stash lhs path
+			# Stash lhs path.
 			f = open(state, 'w')
 			f.write(sys.argv[1])
 			f.close
-
-			# rewrite the 
+			# Rewrite the action for the rhs.
+			# Be liberal in what we accept, and conservative in what we generate.
 			lines = [line for line in open(act_lhs, 'r')]
-			pat = r'^Name=.*'
+			pat = r'^([\ \t])*Name([\ \t])*=.*'
 			lines = [re.sub(pat,'Name=Compare to '+os.path.basename(sys.argv[1]), line) for line in lines]
-			pat = r'^Icon-Name=.*'
+			pat = r'^([\ \t])*Icon-Name([\ \t])*=.*'
 			lines = [re.sub(pat,'Icon-Name=bcomparefull32', line) for line in lines]
-			pat = r'^Comment=.*'
+			pat = r'^([\ \t])*Comment=([\ \t])*.*'
 			lines = [re.sub(pat,'Comment=Select right side for comparison', line) for line in lines]
 			f = open(act_rhs, 'w')
 			for line in lines:
 				f.write(line)
 			f.close()
-
 			os.unlink(act)
 			os.symlink(act_rhs, act)
 else:
